5540443da281f65848b10a1eb59c6bc48f160912
huongcaohao <huongcaoha1994@gmail.com>
2026-05-05 11:05:03 +0700

    update template tham khao

--- src/main/java/org/example/project_base_test/model/entity/User.java
+++ src/main/java/org/example/project_base_test/model/entity/User.java
@@ -7,15 +7,13 @@ import lombok.*;
 @NoArgsConstructor
 @Getter
 @Setter
-@Entity
-@Table(name = "users")
 @Builder
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private String fullName;
     @Column(unique = true)
     private String username;
-
+    private String role ;
+    private String password;
 }
